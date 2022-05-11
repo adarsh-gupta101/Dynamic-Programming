767951c40dec6de3dfb617f60a3fcd1f392c87b8
Adarsh <adarshgupta2468@gmail.com>
2022-05-11 22:39:28 +0530

    can construct

---
+++ canConstruct.js
@@ -0,0 +1,27 @@
+const canConstruct = (target, wordbank,memo=[]) => {
+    if(target in memo){return memo[target]}
+  if (target == "") return true;
+  for (let words of wordbank) {
+    if (target.indexOf(words) === 0) {
+      const suffix = target.slice(words.length);
+      //   console.log(suffix)
+      if (canConstruct(suffix, wordbank,memo) == true) {
+memo[target]=true
+return memo[target]      }
+    }
+  }
+  memo[target]=false
+  return false;
+};
+
+console.log(
+  canConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", [
+    "eeeeeee",
+    "eeeeeeeeeeeeeeeeeee",
+    "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+    "ee",
+    "e",
+    "eee",
+    "eeeeee",
+  ])
+);
